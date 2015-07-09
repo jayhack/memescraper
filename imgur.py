d46740623afb95986e2d432300565d78441e746f
Jay Hack <jhack@stanford.edu>
2015-07-09 15:34:02 +0200

    now scraping based on time

--- imgur.py
+++ imgur.py
@@ -77,7 +77,8 @@ class ImgurScraper(object):
 
 	def get_gallery_page(self, page, verbose=True):
 		"""returns list of GalleryImages"""
-		return self.client.memes_subgallery(sort='top', page=page, window="day")
+		# return self.client.memes_subgallery(sort='top', page=page, window="day")
+		return self.client.memes_subgallery(sort='time', page=page)		
 
 	def scrape_memes(self, page_ix=0):
 		memes = []
